ad7c2edb832bab78d7ecf9da52deaf3cb8f79ddc
joshmarinacci <joshua@marinacci.org>
2025-08-01 14:05:08 -0700

    update hello add info for printing system info

--- src/bin/info.rs
+++ src/bin/info.rs
@@ -6,14 +6,16 @@
     holding buffers for the duration of a data transfer."
 )]
 use esp_hal::clock::CpuClock;
-use esp_hal::main;
+use esp_hal::{chip, main};
 use esp_hal::time::{Duration, Instant};
+use esp_hal::efuse::Efuse;
 use log::info;
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
+
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
@@ -32,7 +34,14 @@ fn main() -> ! {
     info!("running");
 
     loop {
-        info!("Hello world!");
+        info!("getting information!");
+        info!("flash encryption? {}", Efuse::flash_encryption());
+        info!("block version? {:?}", Efuse::block_version());
+        info!("mac address? {:?}", Efuse::mac_address());
+        info!("chip name {:?}", chip!());
+        info!("Free memory {:?}",  esp_alloc::HEAP.free());
+        info!("Used memory {:?}",  esp_alloc::HEAP.used());
+        info!("HEAP stats {}",  esp_alloc::HEAP.stats());
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
     }
