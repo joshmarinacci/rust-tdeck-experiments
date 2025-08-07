0c85ec2569556e7727adb8eafea9a16b6c57afc4
joshmarinacci <joshua@marinacci.org>
2025-08-06 19:09:58 -0700

    make SDCard example work

--- src/bin/sdcard.rs
+++ src/bin/sdcard.rs
@@ -9,6 +9,7 @@
     holding buffers for the duration of a data transfer."
 )]
 
+use alloc::string::String;
 use embedded_hal_bus::spi::ExclusiveDevice;
 use esp_hal::clock::CpuClock;
 use esp_hal::efuse::Efuse;
@@ -16,10 +17,13 @@ use esp_hal::time::{Duration, Instant, Rate};
 use esp_hal::{chip, main};
 use log::info;
 use embedded_sdmmc::{SdCard, VolumeManager, Mode, VolumeIdx, TimeSource, Timestamp};
+use embedded_sdmmc::Mode::ReadOnly;
 use esp_hal::delay::Delay;
-use esp_hal::gpio::{Output, OutputConfig, OutputPin};
-use esp_hal::gpio::Level::Low;
+use esp_hal::gpio::{Input, InputConfig, Output, OutputConfig, OutputPin, Pull};
+use esp_hal::gpio::Level::{High, Low};
 use esp_hal::spi::{ master::{Spi, Config as SpiConfig } };
+use esp_wifi::wifi::event::handle;
+
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
@@ -57,62 +61,70 @@ fn main() -> ! {
 
     esp_alloc::heap_allocator!(size: 72 * 1024);
 
+    let BOARD_POWERON = peripherals.GPIO10;
+    let BOARD_SDCARD_CS = peripherals.GPIO39;
+    let RADIO_CS_PIN = peripherals.GPIO9;
+    let BOARD_TFT_CS = peripherals.GPIO12;
+    let BOARD_SPI_MISO = peripherals.GPIO38;
+
+    let BOARD_SPI_SCK = peripherals.GPIO40;
+    let BOARD_SPI_MOSI = peripherals.GPIO41;
+
     info!("running");
     let mut delay = Delay::new();
+    let mut board_power = Output::new(BOARD_POWERON, High, OutputConfig::default());
+    board_power.set_high();
+    delay.delay_millis(2000);
 
     info!("connecting to the bus");
 
-    let sdmmc_cs = Output::new(peripherals.GPIO39, Low, OutputConfig::default());
+    let sdmmc_cs = Output::new(BOARD_SDCARD_CS, High, OutputConfig::default());
+    let radio_cs = Output::new(RADIO_CS_PIN, High, OutputConfig::default());
+    let board_tft = Output::new(BOARD_TFT_CS, High, OutputConfig::default());
+
+    let board_spi_miso = Input::new(BOARD_SPI_MISO, InputConfig::default().with_pull(Pull::Up));
+
     let sdmmc_spi_bus = Spi::new(peripherals.SPI2,
                                  SpiConfig::default()
                                      .with_frequency(Rate::from_mhz(40))
                                      // .with_mode()
                                      // .with_clock_source(SpiClockSource::LSI)
-    ).unwrap();
-// https://github.com/Xinyuan-LilyGO/T-Deck/blob/master/examples/I2SPlay/utilities.h
-//         .with_sck(peripherals.GPIO40)
-//         .with_miso(peripherals.GPIO38)
-//         .with_mosi(peripherals.GPIO41)
-        // .with_miso(peripherals.GPIO39)
+    ).unwrap()
+        .with_sck(BOARD_SPI_SCK)
+        .with_miso(board_spi_miso)
+        .with_mosi(BOARD_SPI_MOSI)
         ;
-    info!("setup pins");
-
     let sdmmc_spi = ExclusiveDevice::new_no_delay(sdmmc_spi_bus, sdmmc_cs).expect("Failed to create SpiDevice");
     info!("open the card");
     let card = SdCard::new(sdmmc_spi, delay);
     info!("open the volume manager");
-
     let mut volume_mgr = VolumeManager::new(card, DummyTimesource {});
     info!("getting volume");
-
-    // // Try and access Volume 0 (i.e. the first partition).
-    // // The volume object holds information about the filesystem on that volume.
-    let volume = volume_mgr
-        .open_volume(VolumeIdx(0))
-        .expect("Failed to open volume");
-    info!("set up the sdmmc_spi");
-    info!("volume 0: is {:?}",volume);
-
-
-    // // Open the root directory (mutably borrows from the volume).
-    // let root_dir = volume0.open_root_dir()?;
-    // // Open a file called "MY_FILE.TXT" in the root directory
-    // // This mutably borrows the directory.
-    // let my_file = root_dir.open_file_in_dir("MY_FILE.TXT", Mode::ReadOnly)?;
-    // // Print the contents of the file, assuming it's in ISO-8859-1 encoding
-    // while !my_file.is_eof() {
-    //     let mut buffer = [0u8; 32];
-    //     let num_read = my_file.read(&mut buffer)?;
-    //     for b in &buffer[0..num_read] {
-    //         info!("{}", *b as char);
-    //     }
-    // }
-    //
-    // let mut flash = FlashStorage::new();
-    // let flash_addr = 0x9000;
-    // info!("Flash size = {}", flash.capacity());
+    match volume_mgr.open_volume(VolumeIdx(0)) {
+        Ok(handle) => {
+            info!("opened the volume {:?}",handle);
+            let root_dir = handle.open_root_dir().unwrap();
+            root_dir.iterate_dir(|de|{
+                info!("dir entry {:?} is {} bytes",de.name, de.size);
+            }).unwrap();
+            let my_file = root_dir.open_file_in_dir("README.MD", ReadOnly).unwrap();
+            while !my_file.is_eof() {
+                let mut buffer = [0u8; 32];
+                let num_read = my_file.read(&mut buffer).unwrap();
+                let slice = &buffer[0..num_read];
+                let line = String::from_utf8_lossy(slice);
+                info!("{}",line);
+            }
+            my_file.close().unwrap();
+            root_dir.close().unwrap();
+            handle.close().unwrap();
+        }
+        Err(err) => {
+            info!("failed to open the volume {:?}",err);
+        }
+    }
     loop {
         let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        while delay_start.elapsed() < Duration::from_millis(5000) {}
     }
 }
